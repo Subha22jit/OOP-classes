8ef31e517c21e94e6e73d17234213e0986490fb7
Sayan Kundu <sayanisflame@gmail.com>
2020-12-03 11:32:02 +0530

    Made code better

--- codes/Assignment7/Question10.java
+++ codes/Assignment7/Question10.java
@@ -2,16 +2,26 @@ import java.util.Scanner;
 
 public class Question10 {
     static String deleteMe(String str, int m) {
-        char c[] = str.toCharArray();
-        c[m] = '\0';
-        return new String(c);
+        char c2[] = new char[str.length()-1];
+        int add=0;
+        for(int i=0;i<str.length();i++){
+            if(i!=m){
+                c2[i-add]=str.charAt(i);
+            }
+            else{
+                add++;
+            }
+        }
+        return new String(c2);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next() + sc.nextLine();
         int m = sc.nextInt();
+        System.out.println(str.length());
         str = deleteMe(str, m - 1);
+        System.out.println(str.length());
         System.out.println(str);
     }
 }
