62c7ae2e10aa97b4dc14179e34401d9470266f21
Sayan Kundu <sayanisflame@gmail.com>
2020-12-03 14:07:44 +0530

    Done

---
+++ codes/Assignment9/Question4.java
@@ -0,0 +1,11 @@
+public class Question4 {
+    public static void main(String[] args) {
+        StringBuffer sb = null;
+        try {
+            sb.reverse();
+        } catch (NullPointerException e) {
+            System.out.printf("NullPointerException in : Line %s, %s:%s", e.getStackTrace()[0].getLineNumber(),
+                    e.getStackTrace()[0].getClassName(), e.getStackTrace()[0].getMethodName());
+        }
+    }
+}
