26428a7de523d7d92b424e5b0d180ee63cc3b0f1
Sayan Kundu <sayanisflame@gmail.com>
2020-12-17 22:27:20 +0530

    Added

---
+++ codes/Assignment10/Question3.java
@@ -0,0 +1,26 @@
+public class Question3 extends Thread {
+    Question3(String naam) {
+        super(naam);
+    }
+
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(this + " [Timed: " + (i + 1) + "]");
+            // try {
+            // this.sleep(1000);
+            // } catch (Exception e) {
+            // System.out.println(e);
+            // }
+        }
+    }
+
+    public static void main(String[] args) {
+        Question3 qs[] = new Question3[5];
+        for (int i = 1; i < 6; i++) {
+            qs[i - 1] = new Question3("Named Number #" + i);
+            qs[i - 1].setPriority(i);
+            qs[i - 1].start();
+        }
+    }
+
+}
---
+++ notes/Multithreading.md
@@ -0,0 +1,32 @@
+## What is thread?
+
+Each part of a program is a thread
+
+## Advantage of multi thread over single thread?
+
+Provides optimized times
+
+# Thread Schedular
+
+JVM has this  
+It takes decision which thread will execute next.  
+main() to us is a method, but to JVM, it is a thread.  
+Each program has atleast 1 program, that is main.  
+
+## Advantages of multi thread
+
+- Time is optimized
+
+## Disadvantages of multi Thread
+
+- Synchronization problem
+- Deadlocks, Starvation, etc etc..
+
+# Thread Lifecycle
+
+diagram needed to be added
+
+## There are 2 ways to create a new thread 
+
+1. Extends Thread
+2. Implements Runnable
